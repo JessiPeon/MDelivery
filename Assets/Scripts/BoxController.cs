6a79de60df4fb54bfc87703a23c98f38c71347f4
Jessica Peón <jessicapeon17@gmail.com>
2021-12-29 00:31:02 -0300

    version1
    disparo cajas y detecto cuando llegan
    pendiente de forma masiva

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+
+    public float speed = 6;
+    public float direction = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Vector2.right * speed * Time.deltaTime);
+        Destroy(gameObject, 5);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("House"))
+        {
+            other.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            speed = 0;
+        }
+    }
+}
--- Assets/Scripts/StreetScroller.cs
+++ Assets/Scripts/StreetScroller.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 
 public class StreetScroller : MonoBehaviour
 {
-    public BoxCollider2D collider;
     public Rigidbody2D rb;
+    //public GameObject powerUp;
     public GameObject pos1;
     public GameObject pos2;
 
-    private float height;
     private float scrollSpeedY = 2f;
     private float posFinalY;
 
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
-        height = collider.size.y;
-        collider.enabled = false;
         
         rb.velocity = new Vector2(0, scrollSpeedY);
 
@@ -34,6 +30,20 @@ public class StreetScroller : MonoBehaviour
         {
             Vector2 resetPosition = new Vector2(0, pos2.transform.position.y);
             transform.position =  resetPosition;
+            resetPowerUp();
+            foreach (var house in GameObject.FindGameObjectsWithTag("House")) //corregir que aca saco todos 
+            {
+                house.GetComponent<SpriteRenderer>().enabled = false;
+            }
         }
     }
+
+    void resetPowerUp()
+    {
+        if (transform.childCount > 0) { 
+             transform.GetChild(0).localPosition = new Vector3(UnityEngine.Random.Range(-3, 3), 0, 0);
+        }
+    }
+
+    
 }
